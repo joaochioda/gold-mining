d99c60e2a052cd26c88bc5309c511cec7d1fc36d
joaochioda <joaobenetasso@gmail.com>
2025-02-24 09:31:46 -0300

    feat: enhance ShopContainer to display NFTs conditionally based on VIP status and improve loading state management

--- frontend/src/app/(private)/shop/_components/ShopContainer/index.tsx
+++ frontend/src/app/(private)/shop/_components/ShopContainer/index.tsx
@@ -3,18 +3,21 @@
 import { useToast } from "@/hooks/use-toast";
 import { mintNFT, mintVIP } from "@/services/nft";
 import { getCookie } from "@/utils";
-import { Button } from "../../../../../components/ui/button";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Card } from "@/components/Card";
+import { isVip } from "@/services/game";
 
 export default function ShopContainer() {
   const { toast } = useToast();
+  const [isVipState, setIsVipState] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const nfts = [
     {
       name: "Worker",
       price: 1,
       rewardsPerDay: 1,
-      type: "regular",
+      type: 0,
       func: buyNft,
     },
 
@@ -22,21 +25,21 @@ export default function ShopContainer() {
       name: "Machine 1",
       price: 4,
       rewardsPerDay: 4,
-      type: "regular",
+      type: 1,
       func: buyNft,
     },
     {
       name: "Machine 2",
       price: 16,
       rewardsPerDay: 16,
-      type: "regular",
+      type: 2,
       func: buyNft,
     },
     {
       name: "VIP",
       price: 20,
       rewardsPerDay: "+100%",
-      type: "vip",
+      type: 3,
       func: buyVip,
     },
   ];
@@ -76,19 +79,40 @@ export default function ShopContainer() {
     );
   }
 
+  async function getIsVip() {
+    const user = getCookie("address");
+    const vip = await isVip(user!);
+    setIsVipState(vip);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getIsVip();
+  }, []);
+
   return (
     <div>
-      <h1>Shop</h1>
-      <div>
-        {nfts.map((nft, index) => (
-          <div key={index} className="border p-2 m-2 w-[240px]">
-            <p>{nft.name}</p>
-            <p>Price: {nft.price}</p>
-            <p>Rewards per day: {nft.rewardsPerDay}</p>
-            <Button onClick={() => nft.func(index)}>Buy</Button>
-          </div>
-        ))}
-      </div>
+      <h3 className="text-[36px] py-[80px]">SHOP YOUR NFT</h3>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div className="flex flex-wrap gap-9">
+          {nfts.map(
+            (nft, index) =>
+              isVipState && nft.type === 3 ? null : (
+                <Card key={nft.type}>
+                  <Card.Background machine={nft.type} />
+                </Card>
+              )
+            // <div key={index} className="border p-2 m-2 w-[240px]">
+            //   <p>{nft.name}</p>
+            //   <p>Price: {nft.price}</p>
+            //   <p>Rewards per day: {nft.rewardsPerDay}</p>
+            //   <Button onClick={() => nft.func(index)}>Buy</Button>
+            // </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
